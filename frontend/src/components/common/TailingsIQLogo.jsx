9f9961cc649d5409bb549acac7a692f261abd1c8
DRGKuuhaku <crimsonheartleon@gmail.com>
2025-06-25 15:36:17 +1000

    Create TailingsIQLogo.jsx

---
+++ frontend/src/components/common/TailingsIQLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TailingsIQLogo = ({ width = 48, height = 48 }) => (
+  <svg width={width} height={height} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="24" cy="24" r="24" fill="#006039" />
+    <text x="50%" y="55%" textAnchor="middle" fill="#fff" fontSize="18" fontFamily="Arial" dy=".3em">TIQ</text>
+  </svg>
+);
+
+export default TailingsIQLogo;
