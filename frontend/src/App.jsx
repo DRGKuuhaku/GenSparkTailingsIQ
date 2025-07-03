dbeb8e04a729cdfa972ed98150b124916c1878f5
DRGKuuhaku <crimsonheartleon@gmail.com>
2025-07-03 13:40:43 +1000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Box, CircularProgress } from '@mui/material';
+import { Box } from '@mui/material';
 
 import { useAuth } from './contexts/AuthContext';
 import { useUserRole } from './contexts/UserRoleContext';
